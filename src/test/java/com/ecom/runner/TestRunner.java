6c15689f438f7901c55001e55b6d7d07e36af885
Nikhil Kukrety <kukretynikhil4@gmail.com>
2026-03-08 23:49:45 +0530

    Re-stuctured Cucumber reports path

--- src/test/java/com/ecom/runner/TestRunner.java
+++ src/test/java/com/ecom/runner/TestRunner.java
@@ -4,12 +4,12 @@ package com.ecom.runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-//@RunWith(Cucumber.class) -> This is used when we use Cucumber + JUnit framework for running the cucumber tests. 
+//@RunWith(Cucumber.class) -> This is used when we use Cucumber + JUnit framework for running the tests. 
 //This annotation is used to specify that the ExtentReportListener class should be used as a listener for the test execution. The ExtentReportListener class is responsible for generating the Extent Reports based on the test results and events during the execution of the Cucumber tests.
 @CucumberOptions(
 			features = "src/test/resources/features",
 			glue = {"com.ecom.stepDefinitions","com.ecom.hooks"},
-			plugin = {"pretty", "html:target/cucumber/report.html", "json:target/cucumber/cucumber.json"},
+			plugin = {"pretty", "html:target/cucumber-reports.html", "json:target/cucumber-reports.json"},
 			monochrome = true,
 			tags = "@EndToEndTest1"
 			)
